ec17f8be18c0fa7196062f6ea62a17039a2fad95
Akshaya123-ps <144515328+Akshaya123-ps@users.noreply.github.com>
2023-12-02 15:03:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var em=document.getElementById("email");
+var pwd=document.getElementById("password");
+var num=document.getElementById("number");
+var err1=document.getElementById("err1");
+var err2=document.getElementById("err2");
+var err3=document.getElementById("err3");
+
+var regex=/^([A-Za-z0-9\-#_.]+)@([A-Za-z0-9\-]).([a-z]{2,3})(.[a-z]{2,3})?$/
+function validate(){
+    if(em.value.trim()==""){
+        err1.innerText="Field cannot be empty";
+        err1.style.color="red"
+       em.style.border="2px solid red";
+        return  false
+    }
+    else if(!regex.test(em.value)){
+        err1.innerText="Please enter proper email";
+        err1.style.color="red"
+       em.style.border="2px solid red";
+        return  false
+    }
+    else{
+        err1.innerText="";
+        em.style.border="2px solid green"
+        return true
+    }
+}
+
+
+
+
+
+
+
+
